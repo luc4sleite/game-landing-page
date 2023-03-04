5d6e7f07b1ca50769c5ad74d366137007f62e5d3
luc4sleite <lucahleite@gmail.com>
2023-03-04 15:19:45 -0300

    inclusão do js para inserção dos jogos

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const games = [
+  { imageUrl: "./images/mobile/image-elden-ring.jpg", title: "Elden Ring" },
+  { imageUrl: "./images/mobile/image-mwii.jpg", title: "Call of Duty: Modern Warfare 2" },
+  { imageUrl: "./images/mobile/image-fifa.jpg", title: "FIFA 23" },
+];
+
+window.addEventListener("load", main);
+
+function main() {
+    const listGamesElement = document.querySelector(".jogos__lista");
+    games.forEach(game => {
+        const itemList = document.createElement("li");
+        const itemContainer = document.createElement("div");
+        const itemImage = document.createElement("img");
+        const itemTitle = document.createElement("p");
+
+        itemImage.src = game.imageUrl;
+        itemImage.alt = game.title;
+        itemImage.classList.add("jogos__imagem");
+
+        itemTitle.innerHTML = game.title;
+        itemTitle.classList.add("jogos__texto");
+
+        itemContainer.classList.add("itens__container");
+
+        itemList.classList.add("jogos__itens");
+
+        itemContainer.appendChild(itemImage);
+        itemContainer.appendChild(itemTitle);
+        
+        itemList.appendChild(itemContainer);
+
+        listGamesElement.appendChild(itemList);
+
+    })
+}
